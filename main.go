e4a6edea4b3444606f8b37a03fd0f40c35d6b45e
Timur <tima.temirov.2005@mail.ru>
2026-02-26 12:33:07 +0300

    Init server.

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, "<h1>Привет, мир!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
+}
