e113e1a4e546c58de0d1139f6f0cfae80f34f052
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2026-03-07 00:53:07 +0300

    Added tests (20% coverage).

--- main_test.go
+++ main_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-park-mail-ru/2026_1_GPTeam/base"
 	"github.com/go-park-mail-ru/2026_1_GPTeam/jwt"
 	"github.com/go-park-mail-ru/2026_1_GPTeam/middleware"
 	"github.com/go-park-mail-ru/2026_1_GPTeam/storage"
@@ -65,6 +66,57 @@ func TestLogin(t *testing.T) {
 	SetupStorage()
 	mux := SetupRouter()
 	url := "/auth/login"
+
+	for _, tc := range testsCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			body, err := json.Marshal(tc.data)
+			require.NoError(t, err)
+			request, err := http.NewRequest(tc.method, url, bytes.NewBuffer(body))
+			require.NoError(t, err)
+			r := httptest.NewRecorder()
+			mux.ServeHTTP(r, request)
+			require.Equal(t, tc.expectedCode, r.Code)
+		})
+	}
+}
+
+func TestSignup(t *testing.T) {
+	testsCases := []struct {
+		name         string
+		method       string
+		data         map[string]string
+		expectedCode int
+		response     interface{}
+	}{
+		{"get", http.MethodGet, map[string]string{}, http.StatusMethodNotAllowed, nil},
+		{"post invalid", http.MethodPost, map[string]string{"username": "", "password": "", "email": "", "confirm_password": ""}, http.StatusBadRequest, base.SignupErrorResponse{
+			Code:    http.StatusBadRequest,
+			Message: "Неверный формат запроса",
+			Errors: []base.FieldError{
+				{Field: "username", Message: "Поле обязательно для заполнения"},
+				{Field: "password", Message: "Поле обязательно для заполнения"},
+				{Field: "email", Message: "Поле обязательно для заполнения"},
+				{Field: "confirm_password", Message: "Поле обязательно для заполнения"},
+			},
+		}},
+		{"post valid", http.MethodPost, map[string]string{"username": "username", "password": "Admin123", "email": "email@example.com", "confirm_password": "Admin123"}, http.StatusOK, base.SignupSuccessResponse{
+			SimpleResponse: base.SimpleResponse{
+				Code:    http.StatusOK,
+				Message: "Регистрация прошла успешно",
+			},
+			User: base.AuthUser{
+				ID:        1,
+				Username:  "username",
+				Email:     "Admin123",
+				LastLogin: time.Time{},
+				CreatedAt: time.Time{},
+			},
+		}},
+	}
+	SetupStorage()
+	mux := SetupRouter()
+	url := "/signup"
 
 	for _, tc := range testsCases {
 		t.Run(tc.name, func(t *testing.T) {
