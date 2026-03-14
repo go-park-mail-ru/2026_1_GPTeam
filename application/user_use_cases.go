a1403b7171588e259d2414332ae7b27b03d65cba
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2026-03-14 20:57:33 +0300

    Start user use cases.

--- application/user_use_cases.go
+++ application/user_use_cases.go
@@ -1,10 +1,16 @@
 package application
 
-import "github.com/go-park-mail-ru/2026_1_GPTeam/repository"
+import (
+	"context"
+	"time"
+
+	"github.com/go-park-mail-ru/2026_1_GPTeam/base"
+	"github.com/go-park-mail-ru/2026_1_GPTeam/repository"
+	"github.com/go-park-mail-ru/2026_1_GPTeam/storage"
+)
 
 type UserUseCaseInterface interface {
-	Create()
-	Get()
+	Create(ctx context.Context, user base.SignupBodyRequest) (base.AuthUser, error)
 }
 
 type UserUseCase struct {
@@ -15,10 +21,29 @@ func NewUserUseCase(repo repository.UserRepository) *UserUseCase {
 	return &UserUseCase{repo: repo}
 }
 
-func (uc *UserUseCase) Create() {
-
-}
-
-func (uc *UserUseCase) Get() {
-
+func (obj *UserUseCase) Create(ctx context.Context, user base.SignupBodyRequest) (base.AuthUser, error) {
+	avatarUrl := "img/123.png" // ToDo: set default
+	newUser := storage.UserInfo{
+		Id:              0,
+		Username:        user.Username,
+		Password:        user.Password,
+		Email:           user.Email,
+		CreatedAt:       time.Now(),
+		LastLogin:       time.Time{},
+		AvatarUrl:       avatarUrl,
+		Balance:         0,
+		BalanceCurrency: "RUB", // ToDo: delete
+	}
+	id, err := obj.repo.Create(ctx, newUser)
+	if err != nil {
+		return base.AuthUser{}, err
+	}
+	resultUser := base.AuthUser{
+		ID:        id,
+		Username:  newUser.Username,
+		Email:     newUser.Email,
+		LastLogin: newUser.LastLogin,
+		CreatedAt: newUser.CreatedAt,
+	}
+	return resultUser, nil
 }
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -27,7 +27,7 @@ func NewUserRepository(db *pgx.Conn) *UserRepository {
 	return &UserRepository{db: db}
 }
 
-func (r *UserRepository) Create(ctx context.Context, userInfo storage.UserInfo) (int, error) {
+func (obj *UserRepository) Create(ctx context.Context, userInfo storage.UserInfo) (int, error) {
 	query := `insert into "user" (username, password, email, last_login, avatar_url) VALUES ($1, $2, $3, $4, $5) returning id;`
 	var id int
 	username := pgtype.Text{
@@ -55,15 +55,15 @@ func (r *UserRepository) Create(ctx context.Context, userInfo storage.UserInfo) 
 		String: userInfo.AvatarUrl,
 		Valid:  true,
 	}
-	err = r.db.QueryRow(ctx, query, username, password, email, lastLogin, avatarUrl).Scan(&id)
+	err = obj.db.QueryRow(ctx, query, username, password, email, lastLogin, avatarUrl).Scan(&id)
 	if err != nil {
 		fmt.Printf("Unable to create user: %v\n", err)
-		return -1, err
+		return -1, err // ToDo: add err check
 	}
 	return id, nil
 }
 
-func (r *UserRepository) GetById(ctx context.Context, id int) (storage.UserInfo, error) {
+func (obj *UserRepository) GetById(ctx context.Context, id int) (storage.UserInfo, error) {
 	query := `select username, password, email, created_at, last_login, avatar_url, updated_at from "user" where id = $1;`
 	var username pgtype.Text
 	var password pgtype.Text
@@ -72,7 +72,7 @@ func (r *UserRepository) GetById(ctx context.Context, id int) (storage.UserInfo,
 	var lastLogin pgtype.Timestamp
 	var avatarUrl pgtype.Text
 	var updatedAt pgtype.Timestamp
-	err := r.db.QueryRow(ctx, query, id).Scan(&username, &password, &email, &createdAt, &lastLogin, &avatarUrl, &updatedAt)
+	err := obj.db.QueryRow(ctx, query, id).Scan(&username, &password, &email, &createdAt, &lastLogin, &avatarUrl, &updatedAt)
 	if err != nil {
 		fmt.Printf("Unable to get user: %v\n", err)
 		return storage.UserInfo{}, err
@@ -93,7 +93,7 @@ func (r *UserRepository) GetById(ctx context.Context, id int) (storage.UserInfo,
 	return user, nil
 }
 
-func (r *UserRepository) GetByUsername(ctx context.Context, username string) (storage.UserInfo, error) {
+func (obj *UserRepository) GetByUsername(ctx context.Context, username string) (storage.UserInfo, error) {
 	query := `select id, password, email, created_at, last_login, avatar_url, updated_at from "user" where username = $1;`
 	var id pgtype.Int4
 	var password pgtype.Text
@@ -102,7 +102,7 @@ func (r *UserRepository) GetByUsername(ctx context.Context, username string) (st
 	var lastLogin pgtype.Timestamp
 	var avatarUrl pgtype.Text
 	var updatedAt pgtype.Timestamp
-	err := r.db.QueryRow(ctx, query, username).Scan(&id, &password, &email, &createdAt, &lastLogin, &avatarUrl, &updatedAt)
+	err := obj.db.QueryRow(ctx, query, username).Scan(&id, &password, &email, &createdAt, &lastLogin, &avatarUrl, &updatedAt)
 	if err != nil {
 		fmt.Printf("Unable to get user: %v\n", err)
 		return storage.UserInfo{}, err
@@ -123,7 +123,7 @@ func (r *UserRepository) GetByUsername(ctx context.Context, username string) (st
 	return user, nil
 }
 
-func (r *UserRepository) GetByEmail(ctx context.Context, email string) (storage.UserInfo, error) {
+func (obj *UserRepository) GetByEmail(ctx context.Context, email string) (storage.UserInfo, error) {
 	query := `select id, username, password, created_at, last_login, avatar_url, updated_at from "user" where email = $1;`
 	var id pgtype.Int4
 	var username pgtype.Text
@@ -132,7 +132,7 @@ func (r *UserRepository) GetByEmail(ctx context.Context, email string) (storage.
 	var lastLogin pgtype.Timestamp
 	var avatarUrl pgtype.Text
 	var updatedAt pgtype.Timestamp
-	err := r.db.QueryRow(ctx, query, email).Scan(&id, &username, &password, &createdAt, &lastLogin, &avatarUrl, &updatedAt)
+	err := obj.db.QueryRow(ctx, query, email).Scan(&id, &username, &password, &createdAt, &lastLogin, &avatarUrl, &updatedAt)
 	if err != nil {
 		fmt.Printf("Unable to get user: %v\n", err)
 		return storage.UserInfo{}, err
@@ -153,7 +153,7 @@ func (r *UserRepository) GetByEmail(ctx context.Context, email string) (storage.
 	return user, nil
 }
 
-func (r *UserRepository) GetByCredentials(ctx context.Context, username string, password string) (storage.UserInfo, error) {
+func (obj *UserRepository) GetByCredentials(ctx context.Context, username string, password string) (storage.UserInfo, error) {
 	query := `select id, email, created_at, last_login, avatar_url, updated_at from "user" where username = $1 and password = $2;`
 	var id pgtype.Int4
 	var email pgtype.Text
@@ -161,7 +161,7 @@ func (r *UserRepository) GetByCredentials(ctx context.Context, username string, 
 	var lastLogin pgtype.Timestamp
 	var avatarUrl pgtype.Text
 	var updatedAt pgtype.Timestamp
-	err := r.db.QueryRow(ctx, query, username, password).Scan(&id, &email, &createdAt, &lastLogin, &avatarUrl, &updatedAt)
+	err := obj.db.QueryRow(ctx, query, username, password).Scan(&id, &email, &createdAt, &lastLogin, &avatarUrl, &updatedAt)
 	if err != nil {
 		fmt.Printf("Unable to get user: %v\n", err)
 		return storage.UserInfo{}, err
