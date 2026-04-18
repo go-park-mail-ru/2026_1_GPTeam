9d2a0fa0e43bfda74f94bd9110a789413548a7f1
Timur <tima.temirov.2005@mail.ru>
2026-04-18 17:03:10 +0300

    Update.

--- internal/secure/csp.go
+++ internal/secure/csp.go
@@ -8,9 +8,11 @@ import (
 func AddCspHeader(w http.ResponseWriter) {
 	defaultSrc := "'self'"
 	scriptSrc := "'self'"
-	styleSrc := "'self'"
+	styleSrc := "'self' 'unsafe-inline'"
 	imageSrc := "'self' data:"
 	fontSrc := "'self' data:"
-	value := fmt.Sprintf("default-src %s; script-src %s; style-src %s; img-src %s; font-src %s;", defaultSrc, scriptSrc, styleSrc, imageSrc, fontSrc)
+	connectSrc := "'self'"
+	value := fmt.Sprintf("default-src %s; script-src %s; style-src %s; img-src %s; font-src %s; connect-src %s;",
+		defaultSrc, scriptSrc, styleSrc, imageSrc, fontSrc, connectSrc)
 	w.Header().Add("Content-Security-Policy", value)
 }
