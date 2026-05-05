8bf3c2ca7e4b6eb4509a49c031fa0d77cbb84f57
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2026-05-06 02:52:10 +0300

    Updated docker + main.go.

--- main.go
+++ main.go
@@ -65,19 +65,25 @@ func main() {
 		}
 	}()
 
+	metricsPort := os.Getenv("METRICS_PORT")
+	if metricsPort == "" {
+		log.Fatal("METRICS_PORT environment variable not set")
+		return
+	}
+	metricsPort = ":" + metricsPort
 	registry := prometheus.NewRegistry()
 	metrics.InitMetrics(registry)
-	mux2 := http.NewServeMux()
-	mux2.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{Registry: registry}))
-	server2 := &http.Server{
-		Addr:         ":8081",
-		Handler:      mux2,
+	metricsMux := http.NewServeMux()
+	metricsMux.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{Registry: registry}))
+	metricsServer := &http.Server{
+		Addr:         metricsPort,
+		Handler:      metricsMux,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 	go func() {
-		log.Info("starting metrics", zap.String("addr", ":8081"))
-		err = server2.ListenAndServe()
+		log.Info("starting metrics", zap.String("addr", metricsPort))
+		err = metricsServer.ListenAndServe()
 		if err != nil {
 			log.Fatal("Error starting metrics server", zap.Error(err))
 			return
