30013e83836750fa79f2d6d09f51bee496267170
Timur <tima.temirov.2005@mail.ru>
2026-05-14 13:08:53 +0300

    Fix rbac migration.

--- db/migrations/018_rbac_init.sql
+++ db/migrations/018_rbac_init.sql
@@ -3,6 +3,7 @@ create role file_service_role;
 create role ai_service_role;
 create role auth_service_role;
 
+grant usage on schema public to app_service_role, auth_service_role;
 grant insert, delete, select on jwt to auth_service_role;
 grant select on pg_enum to app_service_role;
 grant insert, select, update on "user", budget, transaction, account, support to app_service_role;
@@ -10,7 +11,8 @@ grant insert, select, delete on budget_category, account_user to app_service_rol
 
 ---- create above / drop below ----
 
-drop role if exists app_service_role;
-drop role if exists file_service_role;
-drop role if exists ai_service_role;
-drop role if exists auth_service_role;
+revoke usage on schema public from app_service_role, auth_service_role;
+revoke insert, delete, select on jwt from auth_service_role;
+revoke select on pg_enum from app_service_role;
+revoke insert, select, update on "user", budget, transaction, account, support from app_service_role;
+revoke insert, select, delete on budget_category, account_user from app_service_role;
